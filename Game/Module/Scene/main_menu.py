217320f8c76d02d9023e9bd139156a538afc4772
Angroys <120798951+Angroys@users.noreply.github.com>
2024-12-08 23:59:25 +0200

    Added the Menu class, and implemented the Main Menu scene from it

--- Game/Module/Scene/main_menu.py
+++ Game/Module/Scene/main_menu.py
@@ -1,9 +1,9 @@
 from pygame import Surface,image,transform, font
 from .scene import Scene
-from ..Objects.UI import button, animated_background
+from ..Objects.UI import animated_background, menu
 import asyncio
 
-class MainMenu(Scene):
+class MainMenu(Scene, menu.Menu):
 
     def __init__(self, window: Surface):
         super().__init__(window=window)
@@ -18,44 +18,9 @@ class MainMenu(Scene):
         setting_button_pos =  (2 * (self.height//10.8 )) + initial_button_distance + (2 * btn_distance)
         
         self.play_button = self.create_button(text="Play Game", position=(border_distance, initial_button_distance))
-        self.settings_button = self.create_button(text="Settings", position=(12, self.height//10.8 + initial_button_distance + btn_distance))
-        self.exit_button = self.create_button(text="Exit", position=(12, setting_button_pos))
+        self.settings_button = self.create_button(text="Settings", position=(border_distance, self.height//10.8 + initial_button_distance + btn_distance))
+        self.exit_button = self.create_button(text="Exit", position=(border_distance, setting_button_pos))
         
-
-
-    def create_button(self, text, position, font="Game Assets\casual_passion.ttf"):
-        width, height =self.get_text_size(text)
-        return button.Button(
-            window=self._window,
-            size=(self.width/3.65, self.height//10.8),
-            position= position,
-            text_position=(((self.width//3.65)// 2) - (width// 2) + position[0], ((self.height//10.8)// 2) - (height// 2)+position[1]),
-            text=text,
-            font_size=24,
-            font=font,
-            border_radius=12,
-            colors=[(181, 159, 120), (201, 176, 130), (125, 99,54)],
-            text_colors=[(42,54,99), (53,68,125), (30,39, 72)]
-            
-        )
-
-    def get_text_size(self,text, font_name="Game Assets\casual_passion.ttf", font_size=24):
-        """
-        Returns the width and height of the rendered text for the given font.
-
-        Args:
-            text (str): The text to render.
-            font_name (str): The name of the font (None for default font).
-            font_size (int): The size of the font.
-
-        Returns:
-            tuple: (width, height) of the text rectangle.
-        """
-
-        text_surface = self.font.render(text, True, (0, 0, 0))
-        text_rect = text_surface.get_rect()
-        return text_rect.width, text_rect.height
-
 
     async def loader(self): 
         await self.background.update()
